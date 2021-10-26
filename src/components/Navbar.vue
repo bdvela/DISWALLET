69037b0c0036b7c786a488b6634b2870e7799cf6
bdvela <bdvela@gmail.com>
2021-10-26 16:24:36 -0500

    feat: navbar and home

---
+++ src/components/Navbar.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-card class="mx-auto overflow-hidden" >
+      <v-app-bar app light fixed>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title><span>Diswallet</span></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-list-item>Hola {{usuario}}!</v-list-item>
+          <v-list-item>
+             <router-link to='./myaccount'>
+            <v-avatar color="primary">
+              <v-icon dark>mdi-account</v-icon>
+            </v-avatar>
+             </router-link>
+          </v-list-item>
+        </v-toolbar-items>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" temporary>
+      <v-list nav dense>
+            <v-list-item>
+              <v-layout column align-start>
+                Menu
+              </v-layout>
+            <v-layout column align-end>
+              <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            </v-layout>
+            </v-list-item>
+            <br>
+             <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <router-link to='./home'>
+          <v-list-item>
+            <v-list-item-title>Inicio</v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+          </router-link>
+
+          <router-link to='./discount'>
+          <v-list-item>
+            <v-list-item-title>Descuento de Facturas</v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>mdi-currency-usd</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+          </router-link>
+
+          <router-link to='./myaccount'>
+          <v-list-item>
+            <v-list-item-title>Mi Cuenta</v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+          </router-link>
+          
+          <router-link to='./login'>
+          <v-list-item>
+            <v-list-item-title>Cerrar Sesión</v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({
+    drawer: false,
+    group: null,
+    usuario: 'Peter'
+  }),
+
+};
+</script>
+
+<style scoped>
+
+a {  
+  text-decoration: none;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div> 
+    <Navbar></Navbar>
+    <v-parallax src="../assets/home_wallpaper.jpg"></v-parallax>
+    <v-card shaped elevation="5" class="mx-auto my-12"  max-width="400">
+      <v-card-title class="justify-center c-text"><h3> Descuento de <span>Facturas</span></h3></v-card-title>
+      <v-card-text><v-img class="fact" src="../assets/facturas.png" max-width="150"></v-img></v-card-text>
+      <v-card-actions class="justify-center c-text">
+        <v-btn color="primary" rounded class="btn">Continuar</v-btn> 
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Navbar
+    },
+  }
+</script>
+
+<style scoped>
+
+.fact{
+  margin:auto;
+}
+.c-text{
+  padding:30px;
+}
+
+.btn{
+  width:40%;
+}
+
+.bg{
+  background-color: #f5f5f5;
+}
+</style>
